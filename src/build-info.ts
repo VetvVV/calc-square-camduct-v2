8a3b047375a5cf68e0eac768b322e028a6f0a0e7
Vyacheslav Vetchinov <vyacheslavvetchinov@gmail.com>
2026-06-30 23:38:53 +0300

    Add v2 design foundation and restore light HomePage

--- src/build-info.ts
+++ src/build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
   version: "2.0.0",
   label: "MVP",
-  buildDateTime: "30.06.2026 23:05",
-  commitHash: "2356f3e",
+  buildDateTime: "30.06.2026 23:38",
+  commitHash: "9e24172",
 }
