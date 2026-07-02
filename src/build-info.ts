2568ddb4d27c3f2820871666149d1e8c738bd2f8
Vyacheslav Vetchinov <vyacheslavvetchinov@gmail.com>
2026-07-02 13:53:05 +0300

    Publish v2 UI QA fixes

--- src/build-info.ts
+++ src/build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
   version: "2.0.0",
   label: "MVP",
-  buildDateTime: "02.07.2026 05:43",
-  commitHash: "e8f47b7",
+  buildDateTime: "02.07.2026 13:52",
+  commitHash: "f14dc37",
 }
