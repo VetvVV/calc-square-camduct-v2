f14dc37e0918d84c2e62c56f0365f3749842158d
Vyacheslav Vetchinov <vyacheslavvetchinov@gmail.com>
2026-07-02 05:44:57 +0300

    Publish v2 update

--- src/build-info.ts
+++ src/build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
   version: "2.0.0",
   label: "MVP",
-  buildDateTime: "01.07.2026 16:18",
-  commitHash: "8a3b047",
+  buildDateTime: "02.07.2026 05:43",
+  commitHash: "e8f47b7",
 }
